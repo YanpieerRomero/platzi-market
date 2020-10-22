e7747ae697463f92952172e4e6c43bfa3958ba49
Yanpieer Romero <yanpieer13@gmail.com>
2020-10-21 19:40:35 -0500

    Creando el repositorio de compras

--- src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
@@ -1,10 +1,6 @@
 package com.platzi.market.persistence.entity;
 
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "compras_productos")
@@ -17,12 +13,29 @@ public class ComprasProducto {
     private Boolean estado;
 
     @ManyToOne
+    @MapsId("idCompra")
     @JoinColumn(name = "id_compra", insertable = false, updatable = false)
     private Compra compra;
 
     @ManyToOne
     @JoinColumn(name = "id_producto", insertable = false, updatable = false)
     private Producto producto;
+
+    public Compra getCompra() {
+        return compra;
+    }
+
+    public void setCompra(Compra compra) {
+        this.compra = compra;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
 
     public ComprasProductoPK getId() {
         return id;
